f29ff3c70716d14b23bcad23cfb257091dae11d1
eugeneoei <eugeneoei89@gmail.com>
2021-12-09 14:49:03 +0800

    - change entry file to server.js - added first test case for GET /hello route

---
+++ tests/index.test.js
@@ -0,0 +1,33 @@
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var supertest = require('supertest');
+
+var express = require('express');
+
+describe('GET /hello', () => {
+	let app, request, responseStub, route;
+
+	beforeEach(() => {
+		responseStub = sinon.stub();
+		app = express();
+		route = proxyquire('../controllers/index.js', {});
+        // route = proxyquire
+        route(app);
+		request = supertest(app);
+	});
+
+	it('Should respond with status code 200 and message "world"', (done) => {
+        const expectedRessult = {
+			message: 'world',
+		}
+		responseStub.returns(expectedRessult);
+
+		request
+			.get('/hello')
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				expect(res.body).toEqual(expectedRessult);
+				done();
+			});
+	});
+});

b56e3e5b42cf81c36dea272a230a27528908a400
eugeneoei <eugeneoei89@gmail.com>
2021-12-14 16:10:48 +0800

    simply return values for methods in service since response from users controller unit tests stubs results

--- services/user.service.js
+++ services/user.service.js
@@ -1,46 +1,18 @@
 class UserService {
     createUser(email, firstName, lastName, password) {
-        return {
-            id: '1',
-            email,
-            firstName,
-            lastName,
-        }
+        return 1
     }
 
     getAllUsers() {
-        return [
-            {
-                id: '1',
-                email: 'jon.doe@email.com',
-                firstName: 'Jon',
-                lastName: 'Doe',
-            }
-        ]
+        return 1
     }
 
     getUserById(id) {
-        const users = [
-            {
-                id: '1',
-                email: 'peter.parker@email.com',
-                firstName: 'Peter',
-                lastName: 'Parker',
-            },
-            {
-                id: '2',
-                email: 'mary.jane@email.com',
-                firstName: 'Mary',
-                lastName: 'Jane',
-            },
-            {
-                id: '3',
-                email: 'tony.stark@email.com',
-                firstName: 'Tony',
-                lastName: 'Stark',
-            }
-        ]
-        return users.find(user => user.id === id)
+        return 1
+    }
+
+    updateUserInfo(id) {
+        return 1
     }
 }
 

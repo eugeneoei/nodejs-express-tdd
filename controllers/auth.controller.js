ca8e7b46e8d90d49cf140c97d8e456c191a5cc74
eugeneoei <eugeneoei89@gmail.com>
2021-12-14 19:12:05 +0800

    - implement AuthService and added createUser method - refactor authController to use AuthService to create new user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const UserService = require('../services/user.service')
+const AuthService = require('../services/auth.service')
 
 const authController = (app) => {
     const router = express.Router()
-    const userService = new UserService()
+    const authService = new AuthService()
 
     app.use('/auth', router)
 
     router.post('/register', (req, res) => {
         const { email, firstName, lastName, password } = req.body
-        const user = userService.createUser(email, firstName, lastName, password)
+        const user = authService.createUser(email, firstName, lastName, password)
         res.status(201).json(user)
     })
 }
---
+++ services/auth.service.js
@@ -0,0 +1,8 @@
+class AuthService {
+
+    createUser(email, firstName, lastName, password) {
+        return 1
+    }
+}
+
+module.exports = AuthService

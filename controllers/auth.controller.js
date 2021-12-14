68edfc3a8e3a5e47c9841f680691ab12edd070a7
eugeneoei <eugeneoei89@gmail.com>
2021-12-14 18:55:01 +0800

    refactor POST /auth/register endpoint to pass 'Should create a new user return user object with status code 201' test case

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const UserService = require('../services/user.service')
 
 const authController = (app) => {
     const router = express.Router()
+    const userService = new UserService()
 
     app.use('/auth', router)
 
     router.post('/register', (req, res) => {
-        res.status(201).send()
+        const { email, firstName, lastName, password } = req.body
+        const user = userService.createUser(email, firstName, lastName, password)
+        res.status(201).json(user)
     })
 }
 

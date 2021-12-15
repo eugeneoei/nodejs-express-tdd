76301dfdf24028eb11e303734dcf5d1e2725690b
eugeneoei <eugeneoei89@gmail.com>
2021-12-15 11:12:02 +0800

    wrote test case for createUser method in AuthService where it should call createUser method in UserRepository

--- tests/services/auth.service.test.js
+++ tests/services/auth.service.test.js
@@ -1,4 +1,7 @@
+const sinon = require('sinon')
+
 const AuthService = require('../../services/auth.service')
+const UserRepository = require('../../repositories/user.repository')
 
 describe('Auth Service', () => {
     let authService
@@ -10,5 +13,24 @@ describe('Auth Service', () => {
     it('Should contain the methods createUser and generateTokens', () => {
         expect(authService.createUser).toBeDefined()
         expect(authService.generateTokens).toBeDefined()
+    })
+
+    describe('createUser method', () => {
+        let userRepositoryCreateUserStub
+
+        beforeAll(() => {
+            userRepositoryCreateUserStub = sinon.stub(
+                UserRepository.prototype,
+                'createUser'
+            )
+        })
+
+        afterEach(() => {
+            userRepositoryCreateUserStub.reset()
+        })
+
+        it('Should call createUser method in userRepository and return new user object', () => {
+            expect(userRepositoryCreateUserStub.calledOnce()).toBeTruthy()
+        })
     })
 })

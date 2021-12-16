56d0334be8e8ea98f8ab11d64507edb5b49e71d4
eugeneoei <eugeneoei89@gmail.com>
2021-12-16 23:51:00 +0800

    refactor AuthService - createUser method in to call hashPassword method and pass hashedPassword value to userRepository.createUser - refactor hashPassword method to use bcrypt to generate hashed password

--- services/auth.service.js
+++ services/auth.service.js
@@ -6,12 +6,17 @@ class AuthService {
         this.userRepository = new UserRepository()
     }
 
-    createUser(email, firstName, lastName, password) {
+    async createUser(email, firstName, lastName, password) {
         /**
          * NOTE: could potentially emit a user creation success event. eg subscriber could be welcome email notification
          */
-        const user = this.userRepository.createUser(email, firstName, lastName, password)
+        const hashedPassword = await this.hashPassword(password)
+        const user = this.userRepository.createUser(email, firstName, lastName, hashedPassword)
         return user
+    }
+
+    hashPassword(password) {
+        return bcrypt.hash(password, process.env.SALT_ROUNDS, (err, hash) => hash)
     }
 
     /**

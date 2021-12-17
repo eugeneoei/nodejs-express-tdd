04897b74ef2e0a4c143dc0bbdda60fad9085ae56
eugeneoei <eugeneoei89@gmail.com>
2021-12-17 17:13:19 +0800

    refactor createUser method in UserRepository where user model save method is expected to be called once

--- tests/repositories/user.repository.test.js
+++ tests/repositories/user.repository.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon')
 
 const UserRepository = require('../../repositories/user.repository')
+const User = require('../../models/user.model')
 
 describe('User Repository', () => {
     let userRepository
@@ -17,17 +18,42 @@ describe('User Repository', () => {
     })
 
     describe('createUser method', () => {
+        let userModelSpy
+        let userModelSaveStub
+
+        beforeAll(() => {
+            userModelSaveStub = sinon.stub(User.prototype, 'save')
+            userModelSpy = sinon.spy(User)
+        })
+
+        afterEach(() => {
+            userModelSaveStub.reset()
+        })
 
         it('Should return new user object when called with email, firstName, lastName and password', () => {
-            const expectedResult = {
-                id: '1',
+            const payload = {
                 email: 'jon.doe@email.com',
                 firstName: 'Jon',
-                lastName: 'Doe'
+                lastName: 'Doe',
+                password: 'password1',
             }
-            const response = userRepository.createUser()
+            const expectedResult = {
+                _id: '1',
+                email: 'jon.doe@email.com',
+                firstName: 'Jon',
+                lastName: 'Doe',
+            }
+            userModelSaveStub.returns(expectedResult)
+
+            const response = userRepository.createUser(
+                payload.email,
+                payload.firstName,
+                payload.lastName,
+                payload.password,
+            )
 
             expect(response).toEqual(expectedResult)
-        });
-    });
+            expect(userModelSaveStub.calledOnce).toBe(true)
+        })
+    })
 })

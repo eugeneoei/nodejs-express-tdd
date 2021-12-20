89ad04e65124e1be7d991da2bbb4eb1c013db50b
eugeneoei <eugeneoei89@gmail.com>
2021-12-20 14:56:31 +0800

    add alternative approach to test case and implmentation for user repository createUser method

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -3,14 +3,30 @@ const User = require('../models/user.model')
 class UserRepository {
 
     async createUser(email, firstName, lastName, password) {
+        // Note: document approach
+        // try {
+        //     const newUser = await new User({
+        //         email,
+        //         firstName,
+        //         lastName,
+        //         password
+        //     })
+        //     return newUser.save()
+        // } catch (error) {
+        //     return {
+        //         error: error.message
+        //     }
+        // }
+
+        // Note: model approach
         try {
-            const newUser = await new User({
+            const newUser = await User.create({
                 email,
                 firstName,
                 lastName,
                 password
             })
-            return newUser.save()
+            return newUser
         } catch (error) {
             return {
                 error: error.message
--- tests/repositories/user.repository.test.js
+++ tests/repositories/user.repository.test.js
@@ -1,8 +1,77 @@
-const mockSave = jest.fn()
-const mockUserModel = jest.fn().mockImplementation(() => ({
-    save: mockSave,
+// Note: when using "new" and "save" approach, constructor function is required. "save" is a document method
+// const mockSave = jest.fn()
+// const mockUserModel = jest.fn().mockImplementation(() => ({
+//     save: mockSave,
+// }))
+// jest.mock('../../models/user.model', () => mockUserModel)
+
+// const UserRepository = require('../../repositories/user.repository')
+
+// describe('User Repository', () => {
+//     let userRepository
+
+//     beforeAll(() => {
+//         userRepository = new UserRepository()
+//     })
+
+//     describe('createUser method', () => {
+//         it('Should return new user object', async () => {
+//             const payload = {
+//                 email: 'jon.doe@email.com',
+//                 firstName: 'Jon',
+//                 lastName: 'Doe',
+//                 password: 'password1',
+//             }
+//             const expectedUser = {
+//                 _id: '1',
+//                 email: 'jon.doe@email.com',
+//                 firstName: 'Jon',
+//                 lastName: 'Doe',
+//             }
+//             mockSave.mockImplementationOnce(() => expectedUser)
+
+//             const user = await userRepository.createUser(
+//                 payload.email,
+//                 payload.firstName,
+//                 payload.lastName,
+//                 payload.password
+//             )
+
+//             expect(mockUserModel).toHaveBeenCalledTimes(1)
+//             expect(mockSave).toHaveBeenCalledTimes(1)
+//             expect(user).toEqual(expectedUser)
+//         })
+
+//         it('should throw an error when new instance of User fails', async () => {
+//             const payload = {
+//                 email: 'jon.doe@email.com',
+//                 firstName: 'Jon',
+//                 lastName: 'Doe',
+//             }
+//             mockUserModel.mockImplementationOnce(() => {
+//                 throw new Error('Something went wrong!')
+//             })
+//             const expectedError = {
+//                 error: 'Something went wrong!',
+//             }
+
+//             const user = await userRepository.createUser(
+//                 payload.email,
+//                 payload.firstName,
+//                 payload.lastName
+//             )
+
+//             expect(user).toEqual(expectedError)
+//         })
+//     })
+// })
+
+
+// Note: when using "create" approach does not require constructor function. "create" is a model method
+const mockCreate = jest.fn()
+jest.mock('../../models/user.model', () => ({
+    create: mockCreate
 }))
-jest.mock('../../models/user.model', () => mockUserModel)
 
 const UserRepository = require('../../repositories/user.repository')
 
@@ -27,7 +96,7 @@ describe('User Repository', () => {
                 firstName: 'Jon',
                 lastName: 'Doe',
             }
-            mockSave.mockImplementationOnce(() => expectedUser)
+            mockCreate.mockImplementationOnce(() => expectedUser)
 
             const user = await userRepository.createUser(
                 payload.email,
@@ -36,8 +105,7 @@ describe('User Repository', () => {
                 payload.password
             )
 
-            expect(mockUserModel).toHaveBeenCalledTimes(1)
-            expect(mockSave).toHaveBeenCalledTimes(1)
+            expect(mockCreate).toHaveBeenCalledTimes(1)
             expect(user).toEqual(expectedUser)
         })
 
@@ -47,7 +115,7 @@ describe('User Repository', () => {
                 firstName: 'Jon',
                 lastName: 'Doe',
             }
-            mockUserModel.mockImplementationOnce(() => {
+            mockCreate.mockImplementationOnce(() => {
                 throw new Error('Something went wrong!')
             })
             const expectedError = {
@@ -77,28 +145,28 @@ describe('User Repository', () => {
 
     //     it('Should return user that matches given email string', async () => {
     //         const payload = 'jon.doe@email.com'
-    //         const expectedResult = {
+    //         const expectedUser = {
     //             _id: '1',
     //             email: 'jon.doe@email.com',
     //             firstName: 'Jon',
     //             lastName: 'Doe',
     //         }
-    //         userModelFindOneStub.returns(expectedResult)
+    //         mockFindOne.mockImplementationOnce(() => expectedUser)
 
     //         const response = await userRepository.getUserByEmail(payload)
 
-    //         expect(userModelFindOneStub.calledOnce).toBe(true)
+    //         expect(mockFindOne).toHaveBeenCalled
     //         expect(response).toBe(expectedResult)
     //     })
 
-    //     it('Should return null if user is not found based on given email string', async () => {
-    //         const payload = 'hulk@email.com'
-    //         const expectedResult = null
-    //         userModelFindOneStub.returns(expectedResult)
+    //     // it('Should return null if user is not found based on given email string', async () => {
+    //     //     const payload = 'hulk@email.com'
+    //     //     const expectedResult = null
+    //     //     userModelFindOneStub.returns(expectedResult)
 
-    //         const response = await userRepository.getUserByEmail(payload)
+    //     //     const response = await userRepository.getUserByEmail(payload)
 
-    //         expect(response).toBe(expectedResult)
-    //     })
+    //     //     expect(response).toBe(expectedResult)
+    //     // })
     // })
 })
